660c7fadbf76549cf7fa9bcbeb2fa6d4cfc784da
skalam1 <mike.skala@medtronic.com>
2022-11-23 12:17:28 +0200

    print errors

--- client/client.go
+++ client/client.go
@@ -9,40 +9,44 @@ import (
 )
 
 func RunClient(address string, errCh chan error) {
-	defer fmt.Println("client exited")
+	defer fmt.Println("--client exited")
 
-	fmt.Println("client starting...")
+	fmt.Println("--client starting...")
 	tcpAddr, err := net.ResolveTCPAddr("tcp", address)
 	if err != nil {
+		fmt.Println("--client resolve error: ", err.Error())
 		errCh <- err
 		return
 	}
 	conn, err := net.DialTCP("tcp", nil, tcpAddr) // connect to server (address)
 	if err != nil {
+		fmt.Println("--client dial error: ", err.Error())
 		errCh <- err
 		return
 	}
-	fmt.Println("client connected to: Local:", conn.LocalAddr(), "Remote:", conn.RemoteAddr())
+	fmt.Println("--client connected to: Local:", conn.LocalAddr(), "Remote:", conn.RemoteAddr())
 	defer conn.Close()
 
 	// write a message:
 	payload, _ := json.Marshal(server.Msg{Body: "hi there", Id: 1})
 	_, err = conn.Write(payload)
 	if err != nil {
+		fmt.Println("--client write cmd error: ", err.Error())
 		errCh <- err
 		return
 	}
-	fmt.Println("client sent msg, waiting for server reply")
+	fmt.Println("--client sent msg, waiting for server reply")
 
 	// read a json message (from TCP)
 	decoder := json.NewDecoder(bufio.NewReader(conn))
 	var reply server.Msg
 	err = decoder.Decode(&reply)
 	if err != nil {
+		fmt.Println("--client read reply error: ", err.Error())
 		errCh <- err
 		return
 	}
-	fmt.Println("client got reply: ", reply)
+	fmt.Println("--client got reply: ", reply)
 
 	errCh <- nil
 }
--- server/server.go
+++ server/server.go
@@ -13,38 +13,41 @@ type Msg struct {
 }
 
 func RunServer(address string, errCh chan error) {
-	defer fmt.Println("server exited")
+	defer fmt.Println("- server exited")
 
-	fmt.Println("server starting...")
+	fmt.Println("- server starting...")
 	listener, err := net.Listen("tcp", address) // create server
 	if err != nil {
+		fmt.Println("- server listen error: ", err.Error())
 		errCh <- err
 		return
 	}
 	defer listener.Close()
-	fmt.Println("server listening for incoming connections on:", address)
+	fmt.Println("- server listening for incoming connections on:", address)
 
 	conn, err := listener.Accept()
 	if err != nil {
+		fmt.Println("- server accept error: ", err.Error())
 		errCh <- err
 		return
 	}
 	defer conn.Close()
-	fmt.Println("server connected to: Local:", conn.LocalAddr(), "Remote:", conn.RemoteAddr())
+	fmt.Println("- server connected to: Local:", conn.LocalAddr(), "Remote:", conn.RemoteAddr())
 
 	// wait for message
 	decoder := json.NewDecoder(bufio.NewReader(conn))
 	var msg Msg
 	err = decoder.Decode(&msg)
 	if err != nil {
+		fmt.Println("- server decode error: ", err.Error())
 		errCh <- err
 		return
 	}
-	fmt.Println("server got msg: ", msg)
+	fmt.Println("- server got msg: ", msg)
 
 	// test
 	if msg.Body != "hi there" {
-		err = fmt.Errorf("server got unexpected message body: %v", msg.Body)
+		err = fmt.Errorf("- server got unexpected message body: %v", msg.Body)
 		errCh <- err
 		return
 	}
@@ -52,8 +55,9 @@ func RunServer(address string, errCh chan error) {
 	reply := `{"id": 2, "body": "ok"}`
 	if _, err := fmt.Fprintf(conn, reply); err != nil {
 		errCh <- err
+		fmt.Println("- server reply error: ", err.Error())
 		return
 	}
-	fmt.Println("sever replied")
+	fmt.Println("- server replied")
 	errCh <- nil
 }
